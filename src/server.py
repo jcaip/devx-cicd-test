06fc7021bdceaa58ea96c0af73b68205946dfd31
Jesse Cai <jcjessecai@gmail.com>
2017-04-04 23:25:26 -0700

    fuck this

--- src/server.py
+++ src/server.py
@@ -8,7 +8,7 @@ def test():
 
 @app.route('/two')
 def test_two():
-    return "Working, just pushed this!"
+    return "Working, just pushed this!!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
